5d8d2e466f60e4e7a6f7a8dca55c632482409bf5
pablo <pa.romerorgz@gmail.com>
2021-06-01 23:53:53 -0500

    changes

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1338),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'f964c23f3fe08c02b654f4ca8588fd5e'),
+      secret: env("ADMIN_JWT_SECRET", "f964c23f3fe08c02b654f4ca8588fd5e"),
     },
   },
 });
