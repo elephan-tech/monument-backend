8fedf6ac85876d051ef8f365c83b6355a5d1df7e
pablo <pa.romerorgz@gmail.com>
2021-06-10 17:08:12 -0500

    favicon

--- admin/src/components/PageTitle/index.js
+++ admin/src/components/PageTitle/index.js
@@ -2,7 +2,7 @@ import React, { memo } from 'react';
 import { Helmet } from 'react-helmet';
 import PropTypes from 'prop-types';
 
-import favicon from '../../monument_logo-short.png';
+import favicon from '../../assets/monument_logo-short.png';
 
 const PageTitle = ({ title }) => (
   <Helmet title={title} link={[{ rel: 'icon', type: 'image/png', href: favicon }]} />
