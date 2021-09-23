ee4bc3efafa42d0773d90d35639247e170212c39
NA1000APP-ROMUSNP1 <DL-1000-RPO-DEV-ROMUS DL1000RPODEVROMUS@monsanto.com>
2021-09-23 02:55:14 -0500

    fixing s3

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,31 +1,33 @@
 
 
-module.exports = ({ env }) => ({
-  email: {
-    provider: "nodemailer",
-    providerOptions: {
-      service: "gmail",
-      port: 587,
-      auth: {
-        user: "contact.monumentacademydc@gmail.com",
-        pass: "monument_contact",
+module.exports = ({ env }) => {
+  return ({
+    email: {
+      provider: "nodemailer",
+      providerOptions: {
+        service: "gmail",
+        port: 587,
+        auth: {
+          user: "contact.monumentacademydc@gmail.com",
+          pass: "monument_contact",
+        },
+      },
+      settings: {
+        defaultFrom: "contact.monumentacademydc@gmail.com",
+        defaultReplyTo: "info@monumentacademydc.org",
       },
     },
-    settings: {
-      defaultFrom: "contact.monumentacademydc@gmail.com",
-      defaultReplyTo: "info@monumentacademydc.org",
-    },
-  },
-  upload: {
-    provider: 'aws-s3',
-    providerOptions: {
-      accessKeyId: env('AWS_ACCESS_KEY_ID'),
-      secretAccessKey: env('AWS_ACCESS_SECRET'),
-      region: env('AWS_REGION'),
-      params: {
-        Bucket: env('AWS_BUCKET'),
+    upload: {
+      provider: 'aws-s3',
+      providerOptions: {
+        accessKeyId: env('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: env('AWS_ACCESS_SECRET'),
+        region: env('AWS_REGION'),
+        params: {
+          Bucket: env('AWS_BUCKET'),
+        },
+        logger: console
       },
-      logger: console
     },
-  },
-});
+  })
+};
