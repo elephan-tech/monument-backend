1384fedb14649aef635c95039275c70e8776f224
NA1000APP-ROMUSNP1 <DL-1000-RPO-DEV-ROMUS DL1000RPODEVROMUS@monsanto.com>
2021-09-23 02:36:46 -0500

    fixing s3

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,25 +1,29 @@
 const parse = require("pg-connection-string").parse;
 const config = parse(process.env.DATABASE_URL);
 
-module.exports = ({ env }) => ({
-  defaultConnection: "default",
-  connections: {
-    default: {
-      connector: "bookshelf",
-      settings: {
-        client: "postgres",
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        username: config.user,
-        password: config.password,
-        ssl: {
-          rejectUnauthorized: false,
+module.exports = ({ env }) => {
+  console.log({env});
+  console.log({config, dburl:process.env.DATABASE_URL})
+  return ({
+    defaultConnection: "default",
+    connections: {
+      default: {
+        connector: "bookshelf",
+        settings: {
+          client: "postgres",
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          username: config.user,
+          password: config.password,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+        options: {
+          ssl: true,
         },
       },
-      options: {
-        ssl: true,
-      },
     },
-  },
-});
+  })
+};
